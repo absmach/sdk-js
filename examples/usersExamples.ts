802c7f760cae35c5427f859ff4530e31cb7c18d0
Musilah <nataleigh.nk@gmail.com>
2024-03-28 03:31:00 +0300

    fix users service
    Signed-off-by: Musilah <nataleigh.nk@gmail.com>

---
+++ examples/usersExamples.ts
@@ -0,0 +1,210 @@
+import SDK from '../src/sdk'
+
+const defaultUrl = 'http://localhost'
+
+const mySdk = new SDK({
+  usersUrl: defaultUrl + ':9002'
+})
+
+mySdk.users
+  .Create({
+    name: '<name>',
+    credentials: {
+      identity: 'James@email.com',
+      secret: '12345678'
+    }
+  })
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.User(
+  '<userId>',
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.UserProfile(
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.CreateToken(
+  { identity: 'admin@example.com', secret: '12345678' }
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.RefreshToken(
+  { identity: 'userId' },
+  '<refreshToken>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.Update(
+  { id: '<userId>', name: '<userName>' },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.UpdateUserIdentity(
+  { credentials: { identity: '<userIdentity>' } },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.UpdateUserTags(
+  { tags: ['foo', 'bar'] },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.UpdateUserRole(
+  { role: '<user_role>' },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.Disable(
+  { id: '<userId>' },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.Enable(
+  { id: '<userId>' },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.Users(
+  { offset: 0, limit: 10 },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.UpdateUserPassword(
+  '<oldSecret>', '<newSecret>',
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.ListUserChannels(
+  '<userId>',
+  { offset: 0, limit: 10 },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.ListUserThings(
+  '<userId>',
+  { offset: 0, limit: 10 },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.ListUserGroups(
+  '<userId>',
+  { offset: 0, limit: 10 },
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.ResetPasswordRequest(
+  '<email>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+mySdk.users.ResetPassword(
+  '<password>',
+  '<confPass>',
+  '<token>'
+)
+  .then((response: any) => {
+    console.log('response: ', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
