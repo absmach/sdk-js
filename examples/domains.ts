cbafcf87b98a53f9cbd944c4cff0de44bf848a8e
ianmuchyri <ianmuchiri8@gmail.com>
2024-04-05 12:29:44 +0300

    build: update supported types to be exported
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- examples/domains.ts
+++ examples/domains.ts
@@ -7,10 +7,12 @@ const mySdk = new SDK({
   usersUrl: defaultUrl + ':9002'
 })
 
+const token: string = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkb21haW4iOiIiLCJleHAiOjE3MTIzMDg5MjYsImlhdCI6MTcxMjMwNTMyNiwiaXNzIjoibWFnaXN0cmFsYS5hdXRoIiwic3ViIjoiIiwidHlwZSI6MCwidXNlciI6Ijc1MDgxMTAyLWZkNGUtNDFlOS1iOTc0LTRjNGY4NzZjNDRkNyJ9.DzUl6VnMfouCjaz9EspYuI3FvrRsE7tzxclrFxdqW_nUz1QvIHo3MuClrzsLsGz9SeK-x8fALDU4gNR5uwjezA'
+
 mySdk.domains
   .CreateDomain(
-    { name: '<domainName>' },
-    '<token>'
+    { name: 'domain today' },
+    token
   )
   .then((response: any) => {
     console.log('response: ', response)
@@ -19,118 +21,118 @@ mySdk.domains
     console.log(error)
   })
 
-mySdk.domains.UpdateDomain(
-  { name: '<domainName>', id: '<domainID>' },
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.UpdateDomain(
+//   { name: '<domainName>', id: '<domainID>' },
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.Domain(
-  '<domainID>',
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.Domain(
+//   '<domainID>',
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.DomainPermissions(
-  '<domainID>',
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.DomainPermissions(
+//   '<domainID>',
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.Domains(
-  { offset: 0, limit: 10 },
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.Domains(
+//   { offset: 0, limit: 10 },
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.ListUserDomains(
-  '<userID>',
-  { offset: 0, limit: 10 },
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.ListUserDomains(
+//   '<userID>',
+//   { offset: 0, limit: 10 },
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.ListDomainUsers(
-  '<domainID>',
-  { offset: 0, limit: 10 },
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.ListDomainUsers(
+//   '<domainID>',
+//   { offset: 0, limit: 10 },
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.EnableDomain(
-  '<domainID>',
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.EnableDomain(
+//   '<domainID>',
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.DisableDomain(
-  '<domainID>',
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.DisableDomain(
+//   '<domainID>',
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.AddUsertoDomain(
-  '<domainID>',
-  ['<userID>', '<userID>'],
-  'administrator',
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.AddUsertoDomain(
+//   '<domainID>',
+//   ['<userID>', '<userID>'],
+//   'administrator',
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
-mySdk.domains.RemoveUserfromDomain(
-  '<domainID>',
-  ['<userID>', '<userID>'],
-  'administrator',
-  '<token>'
-)
-  .then((response: any) => {
-    console.log('response: ', response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mySdk.domains.RemoveUserfromDomain(
+//   '<domainID>',
+//   ['<userID>', '<userID>'],
+//   'administrator',
+//   token
+// )
+//   .then((response: any) => {
+//     console.log('response: ', response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
