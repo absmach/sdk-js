04ecb3dbcfa24275c05ffba9ce26d163863a6da9
wambuipixel <kiokowambui015@gmail.com>
2024-04-08 17:06:40 +0300

    fix: fix Lint error
    Signed-off-by: wambuipixel <kiokowambui015@gmail.com>

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,20 +1,10 @@
 import Users from './users'
 import Domains from './domains'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Things from './things'
+import Groups from './groups'
 import Invitations from './invitations'
-import Groups from './groups'
-import Channels from './channels'
-=======
-// import Things from './things'
-=======
-import Things from './things'
->>>>>>> 12444a0 (fix: fixing httpAdapter URL)
-import Groups from './groups'
 import Channels from './channels'
 import Messages from './messages'
->>>>>>> e6c2159 (Updating messages files to typescript)
 
 export type {
   User,
@@ -42,50 +32,27 @@ interface SDKConfig {
   domainsUrl?: string
   thingsUrl?: string
   hostUrl?: string
-<<<<<<< HEAD
-  invitationsUrl?: string
-=======
   readersUrl?: string
   httpadapterUrl?: string
->>>>>>> e6c2159 (Updating messages files to typescript)
+  invitationsUrl?: string
 }
 
 class SDK {
   users: Users
   domains: Domains
-<<<<<<< HEAD
-<<<<<<< HEAD
   things: Things
-  invitations: Invitations
-  groups: Groups
-  channels: Channels
-=======
-  // things: Things
-=======
-  things: Things
->>>>>>> 12444a0 (fix: fixing httpAdapter URL)
   groups: Groups
   channels: Channels
   messages: Messages
->>>>>>> e6c2159 (Updating messages files to typescript)
-
+  invitations: Invitations
   constructor ({
     usersUrl = defaultUrl,
     domainsUrl = defaultUrl,
     thingsUrl = defaultUrl,
     hostUrl = defaultUrl,
-<<<<<<< HEAD
-    invitationsUrl = defaultUrl
-  }: SDKConfig = {}) {
-    this.users = new Users({ usersUrl, thingsUrl, hostUrl })
-    this.domains = new Domains({ domainsUrl, usersUrl })
-    this.things = new Things({ thingsUrl, usersUrl })
-    this.invitations = new Invitations(invitationsUrl)
-    this.groups = new Groups({ usersUrl, thingsUrl })
-    this.channels = new Channels({ thingsUrl, usersUrl })
-=======
     readersUrl = defaultUrl,
-    httpadapterUrl = defaultUrl
+    httpadapterUrl = defaultUrl,
+    invitationsUrl = defaultUrl
   }: SDKConfig = {}) {
     this.users = new Users({ usersUrl, thingsUrl, hostUrl })
     this.domains = new Domains({ domainsUrl, usersUrl })
@@ -93,7 +60,7 @@ class SDK {
     this.groups = new Groups({ usersUrl, thingsUrl })
     this.channels = new Channels({ thingsUrl, usersUrl })
     this.messages = new Messages({ readersUrl, httpadapterUrl })
->>>>>>> e6c2159 (Updating messages files to typescript)
+    this.invitations = new Invitations(invitationsUrl)
   }
 }
 
