7e6b0fb04796818c00e67be3f2625e19c0a4f584
wambuipixel <kiokowambui015@gmail.com>
2024-04-14 14:48:03 +0300

    fix: add tests to things.test.ts
    Signed-off-by: wambuipixel <kiokowambui015@gmail.com>

--- tests/things.test.ts
+++ tests/things.test.ts
@@ -2,8 +2,10 @@ import fetchMock, { enableFetchMocks } from 'jest-fetch-mock'
 
 import SDK from '../src/sdk'
 import type {
-  Thing
-  // ThingsPage
+  Thing,
+  ThingsPage,
+  UsersPage,
+  User
 } from '../src/sdk'
 enableFetchMocks()
 
@@ -23,23 +25,22 @@ describe('Things', () => {
       identity: 'thingidentity',
       secret: 'bb7edb32-2eac-4aad-aebe-ed96fe073879'
     },
-    // 'owner': 'bb7edb32-2eac-4aad-aebe-ed96fe073879',
     metadata: {
       domain: 'example.com'
     },
     status: 'enabled'
   }
-  const thingId = 'bb7edb32-2eac-4aad-aebe-ed96fe073879'
-  const channelId = 'bb7edb32-2eac-4aad-aebe-ed96fe073879'
-  // const thing_ids = ["6cba4ea5-5820-4419-b389-86984309ad35","2bb290ff-0cb1-4f06-9da3-aff91c1d039"];
-  // const channel_ids = ["2bb290ff-0cb1-4f06-9da3-aff91c1d039","6cba4ea5-5820-4419-b389-86984309ad35"];
-  // const userIds = ['b9921574-f562-4048-a6bf-295c0036fc2a', 'ce42d80e-9773-49b2-a8c2-6aa748597a92']
-  const thingKey = '12345678'
-  // const channels = [{"name": "channel1", "id": "bb7edb32-2eac-4aad-aebe-ed96fe073879"},
-  //        {"name": "channel2", "id": "bb7edb32-2eac-4aad-aebe-ed96fe073879"}
-  //  ];
-  // const entity_type = "group";
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjU3OTMwNjksImlhdCI6'
+  const user: User = {
+    name: 'userName',
+    id: 'bb7edb32-2eac-4aad-aebe-ed96fe073879',
+    credentials: {
+      identity: 'useridentity'
+    },
+    metadata: {
+      domain: 'example.com'
+    },
+    status: 'enabled'
+  }
   const things = [
     { name: 'thing1', id: 'bb7edb32-2eac-4aad-aebe-ed96fe073879' },
     { name: 'thing2', id: 'bb7edb32-2eac-4aad-aebe-ed96fe073879' }
@@ -48,149 +49,151 @@ describe('Things', () => {
     offset: 0,
     limit: 10
   }
+  const usersPage: UsersPage = {
+    users: [user],
+    total: 2,
+    offset: 0,
+    limit: 10
+  }
+  const thingsPage: ThingsPage = {
+    things: [thing],
+    total: 2,
+    offset: 10,
+    limit: 0
+  }
+  const thingId = 'bb7edb32-2eac-4aad-aebe-ed96fe073879'
+  const channelId = 'bb7edb32-2eac-4aad-aebe-ed96fe073879'
+  const permissions = 'admin'
   const relation = 'administrator'
+  const userIds = ['b9921574-f562-4048-a6bf-295c0036fc2a', 'ce42d80e-9773-49b2-a8c2-6aa748597a92']
+  const thingKey = '12345678'
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjU3OTMwNjksImlhdCI6'
 
   beforeEach(() => {
     fetchMock.resetMocks()
   })
 
-  test(' create a thing and return success', async () => {
+  test('Create should create a thing and return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(thing))
 
     const response = await sdk.things.Create(thing, token)
     expect(response).toEqual(thing)
   })
 
-  test('Create should handle a conflict error', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.Create(thing, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('CreateBulk should create multiple things and return success', async () => {
+  test('CreateThings should create multiple things and return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(things))
 
     const response = await sdk.things.CreateThings(things, token)
     expect(response).toEqual(things)
   })
 
-  test('CreateBulk should handle a conflict error', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(things))
-
-    const response = await sdk.things.CreateThings(things, token)
-    expect(response).toEqual(things)
-  })
-
-  test('Update should update a thing and return success', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.UpdateThing(thing, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('Update should handle a conflict error', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.UpdateThing(thing, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('Get should give a thing and return success', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.Thing(thingId, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('Get should handle a conflict error', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.Thing(thingId, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('Get by channel return a channel a thing is connected and return success', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.ThingsByChannel(channelId, queryParams, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('Get by channel should handle aconflict error', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.ThingsByChannel(channelId, queryParams, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('GetAll should return all things and return success', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(things))
-
-    const response = await sdk.things.Things(queryParams, token)
-    expect(response).toEqual(things)
-  })
-
-  test('Disable should delete a thing and return success', async () => {
+  test('Disable should update a thing and return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(thing))
 
     const response = await sdk.things.Disable(thing, token)
     expect(response).toEqual(thing)
   })
 
-  test('Disable should delete a thing and return success', async () => {
+  test('Enable should update a thing and return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(thing))
 
-    const response = await sdk.things.Disable(thing, token)
+    const response = await sdk.things.Enable(thing, token)
     expect(response).toEqual(thing)
   })
 
-  test('Update should handle a conflict error', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(thing))
-
-    const response = await sdk.things.UpdateThing(thing, token)
-    expect(response).toEqual(thing)
-  })
-
-  test('Update thing secret should update a thing secret and return success', async () => {
+  test('UpdateThingSecret should update a thing secret and return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(thing))
 
     const response = await sdk.things.UpdateThingSecret(thing, token)
     expect(response).toEqual(thing)
   })
 
-  test('Update thing tags should update a thing tags and return success', async () => {
+  test('UpdateThingTags should update a thing tags and return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(thing))
 
     const response = await sdk.things.UpdateThingTags(thing, token)
     expect(response).toEqual(thing)
   })
 
-  test('Update thing secret should handle a conflict error', async () => {
+  test('Thing should retrieve a thing and return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(thing))
 
-    const response = await sdk.things.UpdateThingSecret(thing, token)
+    const response = await sdk.things.Thing(thingId, token)
     expect(response).toEqual(thing)
   })
 
-  test('Identify thing should identify a thing and return success', async () => {
+  test('UpdateThing should update a thing and return success', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(thing))
+
+    const response = await sdk.things.UpdateThing(thing, token)
+    expect(response).toEqual(thing)
+  })
+
+  test('ThingsByChannel should return a list of channels connected to a specific thing and return success', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(thing))
+
+    const response = await sdk.things.ThingsByChannel(channelId, queryParams, token)
+    expect(response).toEqual(thing)
+  })
+
+  test('IdentifyThing should identify a thing and a return success', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(thing))
 
     const response = await sdk.things.IdentifyThing(thingKey)
     expect(response).toEqual(thing)
   })
 
-  //   test('Share thing should share a thing and return success', async () => {
-  //     fetchMock.mockResponseOnce(JSON.stringify(thing))
+  test('ThingsPermissions should update a thing secret and return success', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(permissions))
 
-  //     const response = await sdk.things.ShareThing(thingId, relation, userIds, token)
-  //     expect(response).toEqual(thing)
-  //   })
+    const response = await sdk.things.ThingsPermissions(thingId, token)
+    expect(response).toEqual(permissions)
+  })
 
-  //   test('Connect should connect a thing and return success', async () => {
-  //     fetchMock.mockResponseOnce(JSON.stringify(thing))
+  test('Things should get a list of all things and return success', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(thingsPage))
 
-  //     const response = await sdk.things.Connect(thing, token)
-  //     expect(response).toEqual(thing)
-  //   })
+    const response = await sdk.things.Things(queryParams, token)
+    expect(response).toEqual(thingsPage)
+  })
+
+  test('ListThingUsers should list users linked to a thing and return success', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(usersPage))
+
+    const response = await sdk.things.ListThingUsers(thingId, queryParams, token)
+    expect(response).toEqual(usersPage)
+  })
+
+  test('ShareThing should share a thing and return success', async () => {
+    const shareResponse = {
+      status: 200,
+      message: 'Thing Shared Successfully'
+    }
+    fetchMock.mockResponseOnce(JSON.stringify(shareResponse))
+
+    const response = await sdk.things.ShareThing(thingId, relation, userIds, token)
+    expect(response).toEqual(shareResponse)
+  })
+
+  test('UnShareThing should unshare a thing and return success', async () => {
+    const unshareResponse = {
+      status: 200,
+      message: 'Thing UnShared Successfully'
+    }
+    fetchMock.mockResponseOnce(JSON.stringify(unshareResponse))
+
+    const response = await sdk.things.UnShareThing(thingId, relation, userIds, token)
+    expect(response).toEqual(unshareResponse)
+  })
+
+  test('DeleteThing should delete a thing and return success', async () => {
+    const deleteResponse = {
+      status: 200,
+      message: 'Thing Deleted Successfully'
+    }
+    fetchMock.mockResponseOnce(JSON.stringify(deleteResponse))
+
+    const response = await sdk.things.DeleteThing(thing, token)
+    expect(response).toEqual(deleteResponse)
+  })
 })
