58de466d3b255f832c50e513c8d5f1dce1c6039a
wambuipixel <kiokowambui015@gmail.com>
2025-01-24 19:39:24 +0300

    add copyright and license to more files
    Signed-off-by: wambuipixel <kiokowambui015@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+// Copyright (c) Abstract Machines
+// SPDX-License-Identifier: Apache-2.0
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node'
-}
+  preset: "ts-jest",
+  testEnvironment: "node"
+};
