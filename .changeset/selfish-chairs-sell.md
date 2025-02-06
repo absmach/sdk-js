e2139bb287fd572ec62b6eadda8073d9ba0960b9
ianmuchyri <ianmuchiri8@gmail.com>
2025-02-06 15:41:12 +0300

    update schedule
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

---
+++ .changeset/selfish-chairs-sell.md
@@ -0,0 +1,5 @@
+---
+"@absmach/magistrala-sdk": patch
+---
+
+Update the content of the schedule for the rules engine
--- src/sdk.ts
+++ src/sdk.ts
@@ -65,6 +65,7 @@ export type {
   EntityMemberRole,
   MembersPage,
   Script,
+  Recurring,
   Schedule,
   RuleStatus,
   Rule,
